ad5142fe7dbefe6fcab66021fc94b8dd9134f743
rrickk12 <henriquemg93@gmail.com>
2020-05-17 21:08:23 -0300

    creating a non-verbose version
    removing the verbose log msg

---
+++ advanced/call_center_queue_manager.py
@@ -0,0 +1,121 @@
+import sys, json
+from twisted.python import log
+from twisted.internet import reactor
+from twisted.internet.protocol import ServerFactory, ClientFactory, Protocol
+
+class Call():
+    def __init__(self,id):
+        self.id = id
+        self.state = "new"
+        self.operator = None
+  
+    def set_state(self,state):
+        self.state = state
+
+    def set_operator(self,operator):
+        self.operator = operator
+
+class ServerProtocol(Protocol):
+    response = {}
+
+    def deliver_call(call):
+        operator = operatorStack.pop()
+        call.set_operator(operator)
+        operatorDict[operator]= "ringing"
+        call.set_state("ringing")
+        callDict[operator] = call
+        #log.msg("Call "+str(call.id)+" ringing for operator "+str(operator))
+        response["response"]=("Call "+str(call.id)+" ringing for operator "+str(operator))
+        self.transport.write(json.dumps(response).encode())
+
+    def dataReceived(self, data):
+        #log.msg('Data received {}'.format(data))
+        dataJSON = json.loads(data.decode())
+        if dataJSON["command"] == 'call':
+            call = Call(int(dataJSON['id']))
+            calls[call.id]=call
+            #log.msg("Call "+str(call.id)+" recieved")
+            response["response"]=("Call "+str(call.id)+" recieved")
+            self.transport.write(json.dumps(response).encode())
+            #if there is no operator available
+            if not len(operatorStack):
+                #log.msg("Call "+str(call.id)+" wainting in queue")
+                response["response"]=("Call "+str(call.id)+" wainting in queue")
+                self.transport.write(json.dumps(response).encode())
+                call.set_state("waiting")
+                callQueue.append(call)
+            else:
+                self.deliver_call(call)
+        elif dataJSON["command"] == 'answer':
+            operator = dataJSON['id']
+            operatorDict[operator]="busy"
+            call = callDict[operator]
+            call.set_state("awnsered")
+            #log.msg("Call "+str(call.id)+" answered by operator "+str(operator))
+            response["response"]=("Call "+str(call.id)+" answered by operator "+str(operator))
+            self.transport.write(json.dumps(response).encode())
+            
+        elif dataJSON["command"] == 'reject':
+            operator = dataJSON['id']
+            call = callDict[operator]
+            operatorDict[operator]="available"
+            operatorStack.append(operator)
+            call.set_state("rejected")
+            #log.msg("Call "+str(call.id)+" rejected by operator "+str(operator))
+            response["response"]=("Call "+str(call.id)+" answered by operator "+str(operator))
+            self.transport.write(json.dumps(response).encode())
+            if not operatorStack == []:
+                self.deliver_call(call)
+
+        elif dataJSON["command"] == 'hangup':
+            call=calls[dataJSON['id']]
+            operator=call.operator
+            if call.state == "ringing" or call.state == "waiting":
+                if call.state == "waiting":
+                    callQueue.remove(call)
+                if call.state == "ringing":
+                    operatorDict[operator]= "available"
+                    operatorStack.append(operator)
+                #log.msg("Call "+str(call.id)+" missed")
+                response["response"]=("Call "+str(call.id)+" missed")
+                self.transport.write(json.dumps(response).encode())
+            else:
+                #log.msg("Call "+str(call.id)+" finished and operator "+str(operator)+" available")
+                operatorDict[operator]="available"
+                operatorStack.append(operator)
+                calls[call.id] = None
+                response["response"]=("Call "+str(call.id)+" finished and operator "+str(operator)+" available")
+                self.transport.write(json.dumps(response).encode())
+            if not callQueue == []:
+                call = callQueue[0]
+                callQueue.remove(call)
+                deliver_call(call)
+        #self.transport.write(b"transportando alguma coisa")
+
+    def connectionMade(self):
+        log.msg('Client connection from {}'.format(self.transport.getPeer()))
+
+    def connectionLost(self, reason):
+        log.msg('Lost connection because {}'.format(reason))
+
+
+class ServerFactory(ServerFactory):
+    def buildProtocol(self, addr):
+        return ServerProtocol()
+
+
+operatorDict = {
+"A": "available",
+"B": "available"}
+callDict = {}
+calls = {}
+callQueue = []
+operatorStack = []
+operatorStack.append("B")
+operatorStack.append("A")
+log.startLogging(sys.stdout)
+log.msg('Start your engines...')
+reactor.listenTCP(5678, EchoServerFactory())
+reactor.run()
+
+
---
+++ advanced/command_interpreter.py
@@ -0,0 +1,90 @@
+import sys
+from cmd import Cmd
+from twisted.python import log
+from twisted.internet import reactor, stdio
+from twisted.internet.protocol import ServerFactory, Factory, Protocol
+from twisted.protocols.basic import LineReceiver
+from os import linesep
+
+class Command(Cmd):
+    #create the cmd functions to be passed by stdin
+    #this functions parse the input data and deliver the message as a json format
+    #call <id> {"command": "call", "id": "<id>"}
+    def do_call(self, arg):
+        call_id = arg.split()[0]
+        request = ('{"command": "call", "id": '+call_id+'}')
+        reactor.callFromThread(connect,request)
+    #answer <id> {"command": "answer", "id": "<id>"}
+    def do_answer(self, arg):
+        operator = arg.split()[0]
+        request = ('{"command": "answer", "id": '+'"'+operator+'"'+'}')
+        reactor.callFromThread(connect,request)
+    #reject <id> {"command": "reject", "id": "<id>"}
+    def do_reject(self, arg):
+        operator = arg.split()[0]
+        request = ('{"command": "reject", "id": '+'"'+operator+'"'+'}')
+        reactor.callFromThread(connect,request)
+    #hangup <id> {"command": "hangup", "id": "<id>"}
+    def do_hangup(self, arg):
+        call=arg.split()[0]
+        request = ('{"command": "hangup", "id": '+call+'}')
+        reactor.callFromThread(connect,request)
+
+#this function is the client protocol that connects to te server and send the data over via TCP protocol
+class ClientProtocol(Protocol):
+    #built in variable
+    def __init__(self, dataJSON = None):
+        self.dataJSON = dataJSON
+
+    def dataReceived(self, data):
+        
+        log.msg('Data received {}'.format(data))
+
+        self.transport.loseConnection()
+
+    def connectionMade(self):
+        log.msg('Data sent {}'.format(self))
+        data = self.dataJSON
+        self.transport.write(data.encode())
+        log.msg('Data sent {}'.format(data))
+
+    def connectionLost(self, reason):
+        log.msg('Lost connection because {}'.format(reason))
+
+#This function is the factory that spawns the client process
+class ClientFactory(Factory):
+    protocol = ClientProtocol
+    #built in variable
+    def __init__ (self, dataJSON=None):
+        self.dataJSON = dataJSON
+
+    def startedConnecting(self, connector):
+        log.msg('Started to connect.')
+
+    #build the client that will connect to the server
+    def buildProtocol(self, addr):
+        log.msg('Connected {}'.format(self.dataJSON))
+		
+        return ClientProtocol(self.dataJSON)
+
+    def clientConnectionLost(self, connector, reason):
+        log.msg('Lost connection. Reason: {}'.format(reason))
+
+    def clientConnectionFailed(self, connector, reason):
+        log.msg('Lost failed. Reason: {}'.format(reason))
+
+#this function recives the data parsed by the cmd and create the tcp connection between the client and server
+def connect(data):
+	reactor.connectTCP('localhost', 5678, ClientFactory(data))
+
+def main():
+
+    log.startLogging(sys.stdout)
+    log.msg('Start your engines...')
+    #the cmd.loop and reactor.run is a blocking instruction we run cmd in another thread
+    reactor.callInThread(Command().cmdloop)
+    reactor.run()
+
+
+if __name__ == '__main__':
+    main()
